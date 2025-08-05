b3c40e81e6a4514757668912771908c392619988
jawadd <jawadduet@gmail.com>
2025-08-05 17:40:32 +0500

    feat: add template directory and create base and index HTML files

--- core/settings.py
+++ core/settings.py
@@ -1,6 +1,7 @@
 import json
 from pathlib import Path
 import dj_database_url
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -42,7 +43,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
